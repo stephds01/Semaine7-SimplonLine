5e79d8788d5f2a3e3709862ca3e63b30c3f2b920
stephds01 <stephds01@gmail.com>
2015-11-09 23:14:31 +0100

    enregistrement dernier exo JS

--- CoursJS_OCR/chapitre_6/js/mots.js
+++ CoursJS_OCR/chapitre_6/js/mots.js
@@ -1,50 +1,60 @@
 /**
  * Created by Stéphanie on 08/11/2015.
  */
-//var mot = prompt("Ecrivez un mot :");
 
-//var longueurMot = mot.length;
-//console.log("Le mot " + mot + " contient " + longueurMot + " caractères");
-//console.log("En majuscule, il s'écrit comme cela :" + mot.toUpperCase());
-//console.log("En minuscule, il s'écrit comme cela :" + mot.toLowerCase());
+    //COMPTER NB VOYELLE
+//function compterVoyelles(mot) {
+//    var nbVoyelles = 0;
+//    for (var i = 0; i < mot.length; i++) {
+//        var lettre = mot[i].toLowerCase();
+//        if ((lettre === 'a') || (lettre === 'e') || (lettre === 'i') || (lettre === 'o') || (lettre === 'u') || (lettre === 'y')) {
+//            nbVoyelles++;
+//        }
+//    }
+//    return nbVoyelles;
+//}
+//
+//var nbVoyelles = compterVoyelles(mot);
+//console.log('Il y a ' + nbVoyelles + ' voyelles et ' + (mot.length - nbVoyelles) + ' consonnes ');
+
+
+//MOT INVERSER
+
+    //function inverser(mot){
+    //
+    //    var motInverse = "";
+    //
+    //    for(var i=0; i<mot.length; i++){
+    //        motInverse = mot[i] +  motInverse;
+    //    }
+    //return motInverse;
+    //
+    //}
+
+//MOT Palindrome
+var motSaisi = prompt('Entrez un mot');
+
+var motInverse = inverser(motSaisi);
+console.log("Il s'écrit à l'envers " + inverser(motSaisi));
+    if(motSaisi.toLowerCase() === inverser.toLowerCase() ){
+        console.log('Le mot est un palindrome');
+    }
+    else{
+        console.log('Ce mot n\'est pas un palindrome');
+    }
+
+
+
+
+
 
 var mot = prompt("Ecrivez un mot :");
 
-function compterNbVoyelles(mot){
-    mot = mot.toLowerCase();    //Je transforme mon mot en minuscule
-    var voyelle;
-    var consonne;
+var longueurMot = mot.length;
+console.log("Le mot " + mot + " contient " + longueurMot + " caractères");
+console.log("En majuscule, il s'écrit comme cela :" + mot.toUpperCase());
+console.log("En minuscule, il s'écrit comme cela :" + mot.toLowerCase());
+console.log("Il s'ecrit à l'envers : " + inverser(mot));
+//console.log(motInverse());
 
-    //Je boucle autant que la longueur du mot
-    for(var i = 0; i<= mot.length; i++){
-        console.log(mot[i]);  //1er test 'Les lettres s'affichent'
 
-            //Si le mot est compris entre A et Z alors
-            if( (mot[i] > "a") && (mot[i] < "z") ){
-
-                //Je partage entre les voyelles et les consonnes
-                var voyelle = mot[i];
-                var consonne = mot[i];
-
-                //Si voyelle est égal à 'a,'e,i,o,u,y, alors j'additionne les voyelles
-                if(voyelle === "a" && voyelle === "e" && voyelle === "i" && voyelle === "o" && voyelle === "u" && voyelle === "y"){
-                    nbVoyelle = voyelle + voyelle;
-                    console.log(nbVoyelle);
-                }
-                //Si consonne est Different de  à 'a,'e,i,o,u,y, alors j'additionne les consonnes
-                else if(voyelle != consonne){
-                    nbConsonne = consonne + consonne ;
-                    console.log(nbConsonne);
-
-                }
-
-            }
-            else{
-                    return "Désolé, ce mot n'est pas valide !";
-            }
-    }
-    console.log("Le " + mot + " contient " + voyelle + " voyelles, et " + consonne + "consonnes !");
-
-}
-
-console.log(compterNbVoyelles(mot));
